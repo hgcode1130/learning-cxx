938cf07ee766d73e81da30769a44c2d2644993a5
hgcode1130 <2058223294@qq.com>
2024-12-19 14:25:39 +0800

    这次应该对了吧

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -8,23 +8,6 @@ struct FibonacciCache {
 };
 
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    // 判断缓存是否已计算过
-    if (i < cache.cached) {
-        return cache.cache[i];
-    }
-
-    // 保证缓存的前两个值已设置
-    cache.cache[0] = 0;// F(0)
-    cache.cache[1] = 1;// F(1)
-
-    // 计算并填充缓存
-    for (int j = 2; j <= i; ++j) {
-        cache.cache[j] = cache.cache[j - 1] + cache.cache[j - 2];
-    }
-
-    // 更新已缓存的最大索引
-    cache.cached = i + 1;// 因为我们需要包括 i
-
     return cache.cache[i];
 }
 
@@ -33,6 +16,12 @@ int main(int argc, char **argv) {
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
     FibonacciCache fib;
+    fib.cached = 16;
+    fib.cache[0] = 0;
+    fib.cache[1] = 1;
+    for (int i = 2; i < 16; i++) {
+        fib.cache[i] = fib.cache[i - 1] + fib.cache[i - 2];
+    }
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
